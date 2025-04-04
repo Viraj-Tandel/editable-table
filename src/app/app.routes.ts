3310e3661332f2c71818c64ba5e9615b5d83cda9
viraj tandel <viraj.tandel@github.com>
2025-04-04 23:02:59 +0530

    Route configuration updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,10 @@ export const routes: Routes = [
         path: 'users', component: TableComponent
     },
     {
-        path: 'user/:id', component: TableDetailsComponent
+        path: 'user/:id', loadComponent: () =>
+            import('./components/table-details/table-details.component').then(
+                (m) => m.TableDetailsComponent
+            ),
     },
     {
         path: '**', redirectTo: 'users', pathMatch: 'full'
